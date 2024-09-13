0675abc47bc6c5ca49aaa6ac14a7f13c5569ffda
TrongBinhLe <ltbinhbk2010@gmail.com>
2024-09-13 09:29:52 +0700

    Create the restart app

---
+++ RestartApp/RestartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartAppApp.swift
+//  RestartApp
+//
+//  Created by admin on 13/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct RestartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
