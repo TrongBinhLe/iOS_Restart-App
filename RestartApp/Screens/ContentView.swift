786cf3d71fce465570bca70e54aa814273aa1bcc
TrongBinhLe <ltbinhbk2010@gmail.com>
2024-09-16 13:26:33 +0700

    Let's build up the onboarding screen

--- RestartApp/Screens/ContentView.swift
+++ RestartApp/Screens/ContentView.swift
@@ -8,14 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        if isOnboardingViewActive {
+            OnBoardingView()
+        } else {
+            HomeView()
         }
-        .padding()
     }
 }
 
---
+++ RestartApp/Screens/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  RestartApp
+//
+//  Created by admin on 13/09/2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Home View")
+    }
+}
+
+#Preview {
+    HomeView()
+}
